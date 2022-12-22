7b7dab7945bcc5c7b1cb5ef3d00b7aa08700a6ab
Moss <charlesiddle99@gmail.com>
2022-12-22 05:23:28 +0300

    Safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except(ZeroDivisionError, IndexError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
