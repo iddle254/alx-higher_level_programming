af37361fc9bc3eb726ab9d11f0e11a606f244954
Moss <charlesiddle99@gmail.com>
2023-01-12 05:29:35 +0300

    My int inherits from int

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
-""" New class """
-Rectangle = __import__("9-rectangle").Rectangle
+"""
+Contains the class Square and subclass
 
+*******
+Square class that inherits from Rectangle that inherits Rectangle
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+# class Square(Rectangle):
+#     """A representation of a square"""
+#     def __init__(self, size):
+#         """instantiation of the square"""
+#         self.integer_validator("size", size)
+#         self.__size = size
+#         super().__init__(size, size)
+#
+#     def area(self):
+#         """"returns the area of the square"""
+#         return self.__size ** 2
 
 class Square(Rectangle):
-    """ Class square inherist from rectangle """
+    """ Class square inherits from rectangle """
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
