1aa0592317e99816fd5d0dbf409fca5a90f11fcc
Moss <charlesiddle99@gmail.com>
2023-04-02 07:26:41 +0300

    Recursive binary search

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,23 +3,21 @@
 Find Peak task 6
 """
 
-
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
         return None
 
     size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    peak = binary_search(list_of_integers, 0, size - 1)
+    return list_of_integers[peak]
 
-    mid = int(size / 2)
-    peak = list_of_integers[mid]
-    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-    elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+def binary_search(a, lo, hi):
+    """Recursive binary search of the peak"""
+    if lo >= hi:
+        return lo
+    mid = ((hi - lo) // 2) + lo
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, lo, mid)
     else:
-        return find_peak(list_of_integers[mid + 1:])
+        return binary_search(a, mid + 1, hi)
