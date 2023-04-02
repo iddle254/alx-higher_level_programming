7f49386d3c8d7c1ae2c7de19b7c21e59671b0d19
Moss <charlesiddle99@gmail.com>
2023-04-02 09:59:08 +0300

    what's my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,9 +5,11 @@ Same as Task 0 but using request package
 
 import requests
 
-
-if __name__ == "__main__":
+def show_status():
     response = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
+
+if __name__ == "__main__":
+    show_status()
